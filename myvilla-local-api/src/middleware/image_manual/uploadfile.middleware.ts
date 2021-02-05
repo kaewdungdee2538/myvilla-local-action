b6af60c92432d5a0bea4afb80a4a79c648bcc738
Ouan Office <kaewdungdee2538@gmail.com>
2021-02-05 08:49:36 +0700

    Update Upload File In Action In To Can Save Local Storage Success

--- myvilla-local-api/src/middleware/image_manual/uploadfile.middleware.ts
+++ myvilla-local-api/src/middleware/image_manual/uploadfile.middleware.ts
@@ -1,6 +1,7 @@
+import 'dotenv/config';
 import { extname } from 'path';
 import { StatusException } from "src/utils/callback.status";
-import { mkdir, readdir } from 'fs';
+import { readdir, existsSync, mkdirSync } from 'fs';
 export const imageFileFilter = (req, file, callback) => {
     if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
         return callback(
@@ -21,71 +22,48 @@ export const imageFileFilter = (req, file, callback) => {
 
 export const editFileName = (req, file, callback) => {
     const name = file.originalname.split('.')[0];
+    const current_date = new Date();
+    const year = current_date.getFullYear().toString();
+    const month = current_date.getMonth().toString();
+    const date = current_date.getDate().toString();
+    const hour = current_date.getHours().toString();
+    const minute = current_date.getHours().toString();
+    const second = current_date.getSeconds().toString();
+    const milsec = current_date.getMilliseconds().toString();
+
+    const file_date_name = `${year}${month}${date}_${hour}${minute}${second}${milsec}`
     const fileExtName = extname(file.originalname);
-    return callback(null, `${name}${fileExtName}`);
+    return callback(null, `${name}_${file_date_name}${fileExtName}`);
 };
 
 export const getCurrentDatePathFileSave = (req, file, callback) => {
     const current_date = new Date();
-    const pathAllFiles = './files';
+    const pathAllFiles = process.env.PATHSAVEIMAGE;
     let year = current_date.getFullYear().toString();
     let month = current_date.getMonth().toString();
     let date = current_date.getDate().toString();
-    let currentPath = pathAllFiles;
-    checkDirectory(currentPath, (err, value) => {
+    let currentPath = `${pathAllFiles}/${year}/${month}/${date}`;
+
+    const dir = currentPath;
+    if (!existsSync(dir)) {
+        mkdirSync(dir, {
+            recursive: true
+        });
+    }
+    readdir(currentPath, err => {
         if (err)
-            return callback(directoryIsNotExist(err), null);
-        else {
-            currentPath = `${currentPath}/${year}`;
-            checkDirectory(currentPath, (err2, value2) => {
-                if (err2)
-                    return callback(directoryIsNotExist(err), null);
-                else {
-                    currentPath = `${currentPath}/${month}`
-                    checkDirectory(currentPath, (err3, value3) => {
-                        if (err3)
-                            return callback(directoryIsNotExist(err), null);
-                        else {
-                            currentPath = `${currentPath}/${date}`
-                            checkDirectory(currentPath, (err3, value3) => {
-                                if (err3)
-                                    return callback(directoryIsNotExist(err), null);
-                                else
-                                    return callback(null, currentPath);
-                            });
-                        }
-                    });
-                }
-            });
-        }
+            return callback(
+                new StatusException(
+                    {
+                        error: 'File is not image.',
+                        result: null,
+                        message: 'ต้องเป็นรูปภาพนามสกุล .jpg .jpeg .png เท่านั้น',
+                        statusCode: 400
+                    },
+                    400,
+                ),
+                false
+            );
+        return callback(null, currentPath);
     });
-
-
-
-}
-
-function checkDirectory(currentPath: string, callback: any) {
-    return readdir(currentPath, (err) => {
-        if (err) {
-            mkdir(currentPath, err => {
-                if (err) {
-                    console.log(err);
-                    return callback(err, false);
-                } return callback(null, true);
-            })
-        }
-        console.log('have folder ' + currentPath);
-        return callback(null, true);
-    });
-}
-
-function directoryIsNotExist(str: string) {
-    return new StatusException(
-        {
-            error: str,
-            result: null,
-            message: str,
-            statusCode: 400
-        },
-        400);
 }
