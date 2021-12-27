e1a9b58edea6c931fd74672bcdc276297f40617e
Ouan Office <kaewdungdee2538@gmail.com>
2021-12-27 10:23:03 +0700

    edit api get booking with lpr response

--- myvilla-local-api/src/controller/lpr/b-check-in/b-check-in.service.ts
+++ myvilla-local-api/src/controller/lpr/b-check-in/b-check-in.service.ts
@@ -17,22 +17,24 @@ export class LPRBCheckInService {
     async getBookingInfoWithLPR(@Body() body){
         const company_id = body.company_id;
         const license_plate = body.license_plate;
-        let sql = `select tbv_id,tbv_code
-        ,to_char(tbv_start_datetime,'YYYY-MM-DD HH24:MI:SS') as  booking_start_datetime
-        ,to_char(tbv_end_datetime,'YYYY-MM-DD HH24:MI:SS') as  booking_end_datetime
-        ,tbv_license_plate as license_plate,tbv_contact_person as person_name,tbv_mobile_contact_person as person_mobile
-        ,case when tbv_status = 'Y' then true else false end as action_out_status
+        let sql = `select 
+        tbv.tbv_code
         ,tbv.home_line_id
-        ,mhl.home_line_first_name as host_first_name
-        ,mhl.home_line_last_name as host_last_name
-        ,mhl.home_line_mobile_phone as host_mobile
-        ,tbv.company_id
-        
+        ,mhl.home_id,mh.home_code,mh.home_name,mh.home_address,mh.home_type
+        ,mh.home_data,mh.home_remark
+        ,mh.home_privilege_line_amount,mh.home_privilege_card_amount
+        ,tbv.tbv_start_datetime
+        ,tbv.tbv_end_datetime
+        ,tbv.tbv_license_plate
+        ,tbv.tbv_contact_person
+        ,tbv.tbv_mobile_contact_person
+        ,tbv.tbv_detail,tbv.tbv_data
+        ,tbv.create_by,tbv.create_date
         from t_booking_visitor tbv
-        left join m_home_line mhl
-        on tbv.home_line_id = mhl.home_line_id
-        
-        where current_timestamp < tbv_end_datetime
+        inner join m_home_line mhl on tbv.home_line_id = mhl.home_line_id
+        left join m_home mh on mhl.home_id = mh.home_id
+        where tbv.delete_flag = 'N'
+        and current_timestamp < tbv_end_datetime
         and tbv.company_id = $1
         and tbv_license_plate = $2
         order by tbv_end_datetime desc 
